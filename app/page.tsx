d1c2af1f01efb4a0a1cfe52711e3cb0691c316e1
VivianaS14 <vasorianp@gmail.com>
2023-08-16 17:17:49 -0500

    feat (home): show list of task from context, style grid for task

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import TaskCard from "@/components/TaskCard";
+import { useTasks } from "@context/TaskContext";
+
 export default function Home() {
+  const { tasks } = useTasks();
+
   return (
     <main>
-      <h1>Hello Next.JS</h1>
+      <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </section>
     </main>
   );
 }
