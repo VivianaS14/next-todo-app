a1aa38d754907bb369c75ce75fe9af9efc5092ea
VivianaS14 <vasorianp@gmail.com>
2023-08-17 14:52:26 -0500

    feat (taskCard): add delete function, fix style

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,3 +1,4 @@
+import { useTasksMutations } from "@/context/TaskContext";
 import { Task } from "..";
 import { useRouter } from "next/navigation";
 
@@ -8,15 +9,24 @@ interface Props {
 export default function TaskCard({ task }: Props) {
   const { id, description, title } = task;
   const router = useRouter();
+  const { removeFromTask } = useTasksMutations();
+
+  const handleDelete = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    // Create alert before delete
+    removeFromTask(task);
+  };
 
   return (
     <div
       className="w-80 mx-auto p-4 bg-cyan-800 rounded-md"
       onClick={() => router.push(`/edit/${id}`)}
     >
-      <div>
+      <div className="flex justify-between">
         <h1 className="text-lg">{title}</h1>
-        <button className="bg-red-400 px-4">Delete</button>
+        <button className="bg-red-400 px-4 rounded-sm" onClick={handleDelete}>
+          Delete
+        </button>
       </div>
       <p>{description}</p>
     </div>
